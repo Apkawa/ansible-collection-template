4136fe0c6d981c56ffa31abf22598aa0348ee4b7
apkawa <apkawa@gmail.com>
2022-06-15 16:12:42 +0300

    Fix tests, checks

--- initial.sh
+++ initial.sh
@@ -2,14 +2,13 @@
 set -ex
 PROJECT_NAME=$1
 
-TEMPLATE_PROJECT = 'collection_template'
+TEMPLATE_PROJECT='collection_template'
 
-
-git reset $(git rev-list --max-parents=0 --abbrev-commit HEAD)
+git reset "$(git rev-list --max-parents=0 --abbrev-commit HEAD)"
 find . -type f -exec sed -i -e "s/$TEMPLATE_PROJECT/$PROJECT_NAME/g" {} \;
 find . -type f -exec sed -i -e "s/{{ YEAR }}/$(date +'%Y')/g" {} \;
-mv __example_app__ $PACKAGE_NAME
-rm -f $0
+mv __example_app__ "$PACKAGE_NAME"
+rm -f "$0"
 
 git add .
 git commit -am "Initial"
